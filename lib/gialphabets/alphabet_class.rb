14b61893cc9917500f89221741f70d580d154d1d
bopes <clcblee@gmail.com>
2016-11-13 23:01:10 -0800

    Add alphabet class

---
+++ lib/gialphabets/alphabet_class.rb
@@ -0,0 +1,23 @@
+module GIalphabets
+  class Alphabet
+
+    attr_reader :letters, :word_break, :letter_break
+
+    def initialize(args)
+      @letters = args.fetch(:letters)
+      @word_break = args.fetch(:word_break, ' ')
+      @letter_break = args.fetch(:letter_break, ',')
+    end
+
+    def translate(string)
+      words = string.downcase.split(" ")
+      words.map! do |word|
+        letters = word.split("")
+        letters.map! { |letter| @letters[letter.to_sym] }
+        letters.join(@letter_break)
+      end
+      words.join(@word_break)
+    end
+
+  end
+end
